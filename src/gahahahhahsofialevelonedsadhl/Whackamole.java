41de8b8a6191b1396176cf02a6cf96c9df3e44a5
pigchick3n <league@WTS-iMac-11.attlocal.net>
2018-01-20 11:32:58 -0800

    Whackamole(IP)
    awrygaevagqgerg

--- src/gahahahhahsofialevelonedsadhl/Whackamole.java
+++ src/gahahahhahsofialevelonedsadhl/Whackamole.java
@@ -14,94 +14,112 @@ import javax.swing.JPanel;
 
 public class Whackamole implements ActionListener {
 	JFrame frame1;
-	JButton buutton = new JButton("mole");
+JButton buutton = new JButton("mole");
+	boolean issetup = true;
 	JPanel panel;
 	int rand;
 	JButton button;
-	int scoretracker;
-	
-	//Make a drawButtons method that takes a random number as a parameter. It should make a GUI like this, but the “mole!” location is random.
-	//[Hint: set the size of the frame rather than packing it.]
-public static void main(String[] args) {
-	Whackamole asdfg = new Whackamole();
-	asdfg.DrawButtons();
-}
-public void DrawButtons() {
-	frame1 = new JFrame("Whack a mole");
-	panel = new JPanel();
-	frame1.setVisible(true);
-	frame1.setSize(500, 500);
-	frame1.add(panel);
-	frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	Random rando = new Random();
-	rand = rando.nextInt(40);
-for( int a = 0; a <= 39; a++) {
-	if(a==rand) {
-		panel.add(buutton);
-		buutton.addActionListener(this);
-		
-	}else {
-	button = new JButton();
-	panel.add(button);
-	button.addActionListener(this);
+	int scoretracker = 0;
+	// Make a drawButtons method that takes a random number as a parameter. It
+	// should make a GUI like this, but the “mole!” location is random.
+	// [Hint: set the size of the frame rather than packing it.]
+	public static void main(String[] args) {
+		Whackamole asdfg = new Whackamole();
+		asdfg.DrawButtons();
 	}
-	
-}
+
+	public void DrawButtons() {
+		frame1 = new JFrame("Whack a mole");
+		panel = new JPanel();
+		frame1.setVisible(true);
+		frame1.setSize(500, 500);
+		frame1.add(panel);
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Random rando = new Random();
+		rand = rando.nextInt(40);
 		
-}
-void speak(String words) {
-try {
-Runtime.getRuntime().exec("say " + words).waitFor();
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+		for (int a = 0; a <= 39; a++) {
+			if (a == rand) {
+				panel.add(buutton);
 
 
-//Dispose of the frame and draw the buttons anew.
+			} else {
+				button = new JButton();
+				panel.add(button);
+				button.addActionListener(this);
+			}
+			if(issetup==true) {
+				buutton.addActionListener(this);
+				issetup = false;
+			}
 
-//After they’ve whacked 10 moles, call this endGame() method.
-private void endGame(Date timeAtStart, int molesWhacked) {
-Date timeAtEnd = new Date();
-JOptionPane.showMessageDialog(null, "Your whack rate is " + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
-}
+		}
 
-//Progressively insult them if they hit something other than the mole! button.
-//If they get click an empty button twice, call them an idiot.
-//If they click an empty button three times, call them a moron.
-//If they click an empty button four times, call them a complete waste of atoms.
-//[Use a switch statement for extra amazingness.]
+	}
 
-//Call this playSound() method when they hit a mole.
-private void playSound(String fileName) {
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Dispose of the frame and draw the buttons anew.
+
+	// After they’ve whacked 10 moles, call this endGame() method.
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+	}
+
+	// Progressively insult them if they hit something other than the mole! button.
+	// If they get click an empty button twice, call them an idiot.
+	// If they click an empty button three times, call them a moron.
+	// If they click an empty button four times, call them a complete waste of
+	// atoms.
+	// [Use a switch statement for extra amazingness.]
+
+	// Call this playSound() method when they hit a mole.
+	private void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
 	}
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	if(e.getSource()!=buutton) {
-		scoretracker--;
-		speak1("lol you are an IDIOT! ha ha ha!!");
-		speak1("ha ha ha ha ha you are a MORON!!");
-			speak1("YOU ARE A DISGRACE AND EMBARRASSMENT OF THIS WORLD!");
-	frame1.dispose();
-	new Whackamole().DrawButtons();
-	}
-	else {
-	 new Whackamole().DrawButtons();
-	playSound("339308__kalibrk__jirkascream-2.mp3");
-	scoretracker++;
-	if(scoretracker==10) {
-	endGame(new Date(), 10);	
-	}}}
-		
 
-void speak1(String words) {
-try {
-Runtime.getRuntime().exec("say " + words).waitFor();
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (!(e.getSource() == buutton)) {
+			scoretracker--;
+			System.out.println(scoretracker);
+			
+
+		} else if(e.getSource()== buutton) {
+			scoretracker++;
+			System.out.println(scoretracker);
+		
+		}
+		if (scoretracker >= 10) {
+			endGame(new Date(), 10);
+			frame1.dispose();
+		}
+		if (scoretracker == -2) {
+			speak1("Idiot.");
+		} else if (scoretracker == -3) {
+			speak1("moron.");
+		} else if (scoretracker == -4) {
+			speak1("you are a complete waste of atoms.");
+		}
+		frame1.dispose();
+		this.DrawButtons();
+	}
+
+	void speak1(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }

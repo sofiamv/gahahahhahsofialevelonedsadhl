cda6edbbd4bdb80e49e4a3922860dd020d84464a
sofiamv <League@AdministorsiMac.attlocal.net>
2018-03-24 15:52:21 -0700

    Test
    Pfffft

---
+++ src/gahahahhahsofialevelonedsadhl/Test.java
@@ -0,0 +1,17 @@
+package gahahahhahsofialevelonedsadhl;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Test {
+	JFrame frame = new JFrame();
+	JButton one = new JButton();
+	JButton two = new JButton();
+	JLabel label = new JLabel();
+	JTextField text = new JTextField();
+public static void main(String[] args) {
+	
+}
+}
